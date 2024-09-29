d8f942fba8ee18a9f39bf4335a6e046db42156d6
Guinn <eguinjonathan08@gmail.com>
2024-09-29 07:52:27 +0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const ibm = IBM_Plex_Serif({
+  subsets: ["latin"],
+  weight: ['400', '700'],
+  variable: "--font-ibm-plex-serif",
+});
+
+
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "Horizon is a modern banking platform for everything",
+  icons: {
+    icon: "/icons/logo.svg",
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${ibm.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
